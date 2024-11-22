8064849dfdb2d97d4f232ed9d60363b7d5a300cd
dankito <git@dankito.net>
2024-11-22 22:36:07 +0100

    Added internationalization for toolbar buttons

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/appskeleton/BottomToolbar.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/appskeleton/BottomToolbar.kt
@@ -10,20 +10,23 @@ import androidx.compose.material.icons.outlined.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import net.codinux.accounting.resources.*
+import net.codinux.accounting.resources.Res
 import net.codinux.accounting.ui.composables.ToolbarButton
 import net.codinux.accounting.ui.tabs.MainScreenTab
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun BottomToolbar(selectedTab: MainScreenTab) {
 
     BottomNavigation(Modifier.height(56.dp)) {
-        ToolbarButton(MainScreenTab.Postings, selectedTab, Icons.Outlined.Book, "Buchung")
+        ToolbarButton(MainScreenTab.Postings, selectedTab, Icons.Outlined.Book, stringResource(Res.string.postings))
 
-        ToolbarButton(MainScreenTab.BankAccounts, selectedTab, Icons.Outlined.AccountBalance, "Konto")
+        ToolbarButton(MainScreenTab.BankAccounts, selectedTab, Icons.Outlined.AccountBalance, stringResource(Res.string.bank_accounts))
 
-        ToolbarButton(MainScreenTab.Invoices, selectedTab, Icons.AutoMirrored.Outlined.ReceiptLong, "Rechnung")
+        ToolbarButton(MainScreenTab.Invoices, selectedTab, Icons.AutoMirrored.Outlined.ReceiptLong, stringResource(Res.string.invoices))
 
-        ToolbarButton(MainScreenTab.Mails, selectedTab, Icons.Outlined.Email, "E-Mail")
+        ToolbarButton(MainScreenTab.Mails, selectedTab, Icons.Outlined.Email, stringResource(Res.string.mails))
     }
 
 }

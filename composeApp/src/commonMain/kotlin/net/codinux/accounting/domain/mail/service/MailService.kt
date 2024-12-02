a30e0f7ee0d80ed6998507c1e4fc2339e7a6ae44
dankito <git@dankito.net>
2024-12-02 23:07:32 +0100

    Implemented concurrently fetching new mails

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/domain/mail/service/MailService.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/domain/mail/service/MailService.kt
@@ -1,6 +1,8 @@
 package net.codinux.accounting.domain.mail.service
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.toList
 import net.codinux.accounting.domain.common.model.error.ErroneousAction
 import net.codinux.accounting.domain.mail.dataaccess.MailRepository
 import net.codinux.accounting.domain.mail.model.MailAccountConfiguration
@@ -10,11 +12,13 @@ import net.codinux.accounting.ui.state.UiState
 import net.codinux.invoicing.email.*
 import net.codinux.invoicing.email.model.*
 import net.codinux.log.logger
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MailService(
     private val uiState: UiState,
     private val emailsFetcher: EmailsFetcher,
-    private val repository: MailRepository
+    private val repository: MailRepository,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
 ) {
 
     private val log by logger()
@@ -28,10 +32,10 @@ class MailService(
             uiState.emails.mailAccounts.emit(loadPersistedMailAccounts())
 
             uiState.emails.mailAccounts.value.forEach { account ->
-                withContext(Dispatchers.IO) {
-                    val lastRetrievedMessageId = allEmails.filter { it.emailAccountId == account.id }.maxOfOrNull { it.messageId }
+                val lastRetrievedMessageId = allEmails.filter { it.emailAccountId == account.id }.maxOfOrNull { it.messageId }
 
-                    launch { fetchAndListenToNewMails(account, lastRetrievedMessageId, this) }
+                coroutineScope.launch(Dispatchers.IO) {
+                    fetchAndListenToNewMails(account, lastRetrievedMessageId)
                 }
             }
         } catch (e: Throwable) {
@@ -45,10 +49,12 @@ class MailService(
     // errors handled by init()
     private suspend fun loadPersistedMails() = repository.loadMails()
 
-    private suspend fun persistEmails(account: MailAccountConfiguration, emails: Collection<net.codinux.invoicing.email.model.Email>) {
+    private suspend fun persistEmails(account: MailAccountConfiguration, emails: Collection<Email>) {
         try {
-            val allMails = repository.saveMails(account, emails)
-            uiState.emails.mails.emit(allMails.toList()) // make a copy. otherwise the same instance of MailRepository.allMails would be passed to uiState.mails which therefore cannot detect changes
+            if (emails.isNotEmpty()) {
+                val allMails = repository.saveMails(account, emails)
+                uiState.emails.mails.emit(allMails.toList()) // make a copy. otherwise the same instance of MailRepository.allMails would be passed to uiState.mails which therefore cannot detect changes
+            }
         } catch (e: Throwable) {
             log.error(e) { "Could not persist emails of account ${account.receiveEmailConfiguration}" }
 
@@ -57,13 +63,13 @@ class MailService(
     }
 
 
-    private suspend fun fetchAndListenToNewMails(configuration: MailAccountConfiguration, lastRetrievedMessageId: Long? = null, scope: CoroutineScope) {
+    private suspend fun fetchAndListenToNewMails(configuration: MailAccountConfiguration, lastRetrievedMessageId: Long? = null) {
         configuration.receiveEmailConfiguration?.let { account ->
              fetchAndPersistEmails(configuration, account, lastRetrievedMessageId)
 
             try {
                 emailsFetcher.listenForNewEmails(account, ListenForNewMailsOptions(downloadMessageBody = true, downloadOnlyPlainTextOrHtmlMessageBody = true, onError = { handleFetchEmailError(it) }) { newEmail ->
-                    scope.launch {
+                    coroutineScope.launch {
                         persistEmails(configuration, listOf(newEmail))
                     }
                 })
@@ -78,7 +84,19 @@ class MailService(
     }
 
     private suspend fun fetchAndPersistEmails(configuration: MailAccountConfiguration, fetchAccount: EmailAccount, lastRetrievedMessageId: Long? = null) = try {
-        val result = emailsFetcher.fetchAllEmails(fetchAccount, FetchEmailsOptions(lastRetrievedMessageId, downloadMessageBody = true, downloadOnlyPlainTextOrHtmlMessageBody = true))
+        val channel = Channel<Email>(Channel.UNLIMITED)
+        val isFetchingMails = AtomicBoolean(true)
+
+        coroutineScope.launch(Dispatchers.IO) {
+            persistEmailsBatched(configuration, channel, isFetchingMails)
+        }
+
+        val result = emailsFetcher.fetchAllEmails(fetchAccount, FetchEmailsOptions(lastRetrievedMessageId, downloadMessageBody = true, downloadOnlyPlainTextOrHtmlMessageBody = true) { newEmail ->
+            channel.trySend(newEmail)
+        })
+
+        isFetchingMails.set(false)
+
 
         if (result.overallError != null) {
             uiState.errorOccurred(ErroneousAction.FetchEmails, Res.string.error_message_could_not_fetch_emails, result.overallError)
@@ -86,17 +104,30 @@ class MailService(
         result.messageSpecificErrors.forEach { error ->
             uiState.errorOccurred(ErroneousAction.FetchEmails, Res.string.error_message_could_not_fetch_emails, error.error)
         }
-
-        val newEmails = result.emails
-
-        if (newEmails.isNotEmpty()) {
-            persistEmails(configuration, newEmails)
-        }
-        Unit
     } catch (e: Throwable) {
         log.error(e) { "Could not fetch emails of account $fetchAccount" }
 
         uiState.errorOccurred(ErroneousAction.FetchEmails, Res.string.error_message_could_not_fetch_emails, e)
+    }
+
+    private suspend fun persistEmailsBatched(configuration: MailAccountConfiguration, channel: Channel<Email>, isFetchingMails: AtomicBoolean) {
+        while (isFetchingMails.get() || channel.isEmpty == false) {
+            delay(1_000) // to reduce disk I/O batch received emails and persist them only once a second
+
+            val newEmails = buildList {
+                var result = channel.tryReceive()
+                while (result.isSuccess) {
+                    result.getOrNull()?.let { add(it) }
+
+                    result = channel.tryReceive()
+                }
+            }
+
+            persistEmails(configuration, newEmails)
+        }
+
+        val remainingEmails = channel.toList()
+        persistEmails(configuration, remainingEmails)
     }
 
 

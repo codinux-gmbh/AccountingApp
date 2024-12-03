1bf51e1ba664f924d344a5c98a40a9ee068f9ee8
dankito <git@dankito.net>
2024-12-03 05:36:25 +0100

    Fixed that HistoricalInvoiceData has been removed from InvoiceForm() parameter list

--- composeApp/src/androidMain/kotlin/net/codinux/accounting/ui/composables/invoice/InvoiceFormPreview.kt
+++ composeApp/src/androidMain/kotlin/net/codinux/accounting/ui/composables/invoice/InvoiceFormPreview.kt
@@ -2,10 +2,9 @@ package net.codinux.accounting.ui.composables.invoice
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import net.codinux.accounting.domain.invoice.model.HistoricalInvoiceData
 
 @Preview
 @Composable
 fun InvoiceFormPreview() {
-    InvoiceForm(HistoricalInvoiceData())
+    InvoiceForm()
 }

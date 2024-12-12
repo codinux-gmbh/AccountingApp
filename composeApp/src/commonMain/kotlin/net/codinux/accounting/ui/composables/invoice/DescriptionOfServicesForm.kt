8b5bd3a52ea449614608fd8995083ada4e06a49c
dankito <git@dankito.net>
2024-12-12 08:42:45 +0100

    Extracted ServiceDateForm

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/invoice/DescriptionOfServicesForm.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/invoice/DescriptionOfServicesForm.kt
@@ -6,69 +6,28 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import net.codinux.accounting.domain.invoice.model.ServiceDateOptions
-import net.codinux.accounting.resources.*
 import net.codinux.accounting.resources.Res
 import net.codinux.accounting.resources.delivered_goods_or_provided_services
-import net.codinux.accounting.resources.service_period_end
-import net.codinux.accounting.resources.service_period_start
-import net.codinux.accounting.ui.composables.forms.Select
-import net.codinux.accounting.ui.composables.forms.datetime.DatePicker
-import net.codinux.accounting.ui.composables.forms.datetime.SelectMonth
 import net.codinux.accounting.ui.composables.invoice.model.DescriptionOfServicesViewModel
 import net.codinux.accounting.ui.composables.invoice.model.InvoiceItemViewModel
 import net.codinux.accounting.ui.config.Colors
-import net.codinux.accounting.ui.config.Style
-import net.codinux.accounting.ui.extensions.widthForScreen
 import org.jetbrains.compose.resources.stringResource
-import java.time.LocalDate
-
-
-private val VerticalRowPadding = Style.FormVerticalRowPadding
 
 @Composable
 fun DescriptionOfServicesForm(viewModel: DescriptionOfServicesViewModel, isCompactScreen: Boolean) {
 
-    val selectedServiceDateOption by viewModel.serviceDateOption.collectAsState()
-    val servicePeriodDefaultMonth = LocalDate.now().minusMonths(1)
-    var serviceDate by remember { mutableStateOf(LocalDate.now()) }
-    var servicePeriodMonth by remember { mutableStateOf(servicePeriodDefaultMonth.month) }
-    var servicePeriodStart by remember { mutableStateOf(servicePeriodDefaultMonth.withDayOfMonth(1)) }
-    var servicePeriodEnd by remember { mutableStateOf(servicePeriodDefaultMonth.withDayOfMonth(servicePeriodDefaultMonth.lengthOfMonth())) }
-
     val invoiceItems by viewModel.items.collectAsState()
 
 
-    @Composable
-    fun getLabel(option: ServiceDateOptions): String = when (option) {
-        ServiceDateOptions.DeliveryDate -> stringResource(Res.string.delivery_date)
-        ServiceDateOptions.ServiceDate -> stringResource(Res.string.service_date)
-        ServiceDateOptions.ServicePeriodMonth -> stringResource(Res.string.service_period_month)
-        ServiceDateOptions.ServicePeriodCustom -> stringResource(Res.string.service_period)
-    }
-
-
-    Row(Modifier.fillMaxWidth().padding(top = VerticalRowPadding), verticalAlignment = Alignment.CenterVertically) {
-        Select(null, ServiceDateOptions.entries, selectedServiceDateOption, { viewModel.serviceDateOptionChanged(it) }, { getLabel(it) }, Modifier.padding(end = 8.dp).widthForScreen(isCompactScreen, 185.dp, 210.dp))
-
-        when (selectedServiceDateOption) {
-            ServiceDateOptions.DeliveryDate -> { DatePicker(null, serviceDate) { serviceDate = it } }
-            ServiceDateOptions.ServiceDate -> { DatePicker(null, serviceDate) { serviceDate = it } }
-            ServiceDateOptions.ServicePeriodMonth -> { SelectMonth(servicePeriodMonth) { servicePeriodMonth = it } }
-            ServiceDateOptions.ServicePeriodCustom -> {
-                DatePicker(Res.string.service_period_start, servicePeriodStart, moveFocusOnToNextElementOnSelection = false) { servicePeriodStart = it }
-                Text("-", textAlign = TextAlign.Center, modifier = Modifier.width(18.dp))
-                DatePicker(Res.string.service_period_end, servicePeriodEnd) { servicePeriodEnd = it }
-            }
-        }
-    }
+    ServiceDateForm(viewModel, isCompactScreen)
 
     Column(Modifier.fillMaxWidth()) {
         Row(Modifier.fillMaxWidth().padding(top = 12.dp).height(30.dp), verticalAlignment = Alignment.CenterVertically) {
---
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/invoice/ServiceDateForm.kt
@@ -0,0 +1,59 @@
+package net.codinux.accounting.ui.composables.invoice
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import net.codinux.accounting.domain.invoice.model.ServiceDateOptions
+import net.codinux.accounting.resources.*
+import net.codinux.accounting.ui.composables.forms.Select
+import net.codinux.accounting.ui.composables.forms.datetime.DatePicker
+import net.codinux.accounting.ui.composables.forms.datetime.SelectMonth
+import net.codinux.accounting.ui.composables.invoice.model.DescriptionOfServicesViewModel
+import net.codinux.accounting.ui.config.Style
+import net.codinux.accounting.ui.extensions.widthForScreen
+import org.jetbrains.compose.resources.stringResource
+import java.time.LocalDate
+
+
+private val VerticalRowPadding = Style.FormVerticalRowPadding
+
+@Composable
+fun ServiceDateForm(viewModel: DescriptionOfServicesViewModel, isCompactScreen: Boolean) {
+
+    val selectedServiceDateOption by viewModel.serviceDateOption.collectAsState()
+    val servicePeriodDefaultMonth = LocalDate.now().minusMonths(1)
+    var serviceDate by remember { mutableStateOf(LocalDate.now()) }
+    var servicePeriodMonth by remember { mutableStateOf(servicePeriodDefaultMonth.month) }
+    var servicePeriodStart by remember { mutableStateOf(servicePeriodDefaultMonth.withDayOfMonth(1)) }
+    var servicePeriodEnd by remember { mutableStateOf(servicePeriodDefaultMonth.withDayOfMonth(servicePeriodDefaultMonth.lengthOfMonth())) }
+
+
+    @Composable
+    fun getLabel(option: ServiceDateOptions): String = when (option) {
+        ServiceDateOptions.DeliveryDate -> stringResource(Res.string.delivery_date)
+        ServiceDateOptions.ServiceDate -> stringResource(Res.string.service_date)
+        ServiceDateOptions.ServicePeriodMonth -> stringResource(Res.string.service_period_month)
+        ServiceDateOptions.ServicePeriodCustom -> stringResource(Res.string.service_period)
+    }
+
+
+    Row(Modifier.fillMaxWidth().padding(top = VerticalRowPadding), verticalAlignment = Alignment.CenterVertically) {
+        Select(null, ServiceDateOptions.entries, selectedServiceDateOption, { viewModel.serviceDateOptionChanged(it) }, { getLabel(it) }, Modifier.padding(end = 8.dp).widthForScreen(isCompactScreen, 185.dp, 210.dp))
+
+        when (selectedServiceDateOption) {
+            ServiceDateOptions.DeliveryDate -> { DatePicker(null, serviceDate) { serviceDate = it } }
+            ServiceDateOptions.ServiceDate -> { DatePicker(null, serviceDate) { serviceDate = it } }
+            ServiceDateOptions.ServicePeriodMonth -> { SelectMonth(servicePeriodMonth) { servicePeriodMonth = it } }
+            ServiceDateOptions.ServicePeriodCustom -> {
+                DatePicker(Res.string.service_period_start, servicePeriodStart, moveFocusOnToNextElementOnSelection = false) { servicePeriodStart = it }
+                Text("-", textAlign = TextAlign.Center, modifier = Modifier.width(18.dp))
+                DatePicker(Res.string.service_period_end, servicePeriodEnd) { servicePeriodEnd = it }
+            }
+        }
+    }
+
+}

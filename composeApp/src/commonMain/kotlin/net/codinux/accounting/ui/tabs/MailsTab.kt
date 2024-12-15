6ee3597e5f9e5070ef5d039e02d913002b5c77c2
dankito <git@dankito.net>
2024-12-15 15:48:27 +0100

    Showing hint why an email account should be added if none is added

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/tabs/MailsTab.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/tabs/MailsTab.kt
@@ -1,11 +1,20 @@
 package net.codinux.accounting.ui.tabs
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import net.codinux.accounting.resources.*
 import net.codinux.accounting.ui.composables.mail.MailsList
 import net.codinux.accounting.ui.dialogs.AddEmailAccountDialog
 import net.codinux.accounting.ui.state.EmailsUiState
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun MailsTab(uiState: EmailsUiState) {
@@ -15,7 +24,13 @@ fun MailsTab(uiState: EmailsUiState) {
     val showAddMailAccountDialog by uiState.showAddMailAccountDialog.collectAsState()
 
 
-    MailsList(uiState, mails)
+    if (mails.isEmpty()) {
+        Column(Modifier.fillMaxSize().padding(horizontal = 36.dp), Arrangement.Center, Alignment.CenterHorizontally) {
+            Text(stringResource(Res.string.reason_for_adding_email_account), fontSize = 18.sp, textAlign = TextAlign.Center)
+        }
+    } else {
+        MailsList(uiState, mails)
+    }
 
 
     if (showAddMailAccountDialog) {

71358a3d5aa8e18b9d47c7399b87d94c4e5b8fd8
dankito <git@dankito.net>
2024-12-16 03:08:13 +0100

    Set new app name in Desktop app

--- composeApp/src/desktopMain/kotlin/net/codinux/accounting/main.kt
+++ composeApp/src/desktopMain/kotlin/net/codinux/accounting/main.kt
@@ -4,11 +4,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
+import net.codinux.accounting.resources.*
+import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Accounting",
+        title = stringResource(Res.string.app_name),
         state = WindowState(position = WindowPosition(Alignment.Center), size = DpSize(1000.dp, 800.dp)),
     ) {
         App()

301c9513a6172d767288ed24f554fdb809f66154
dankito <git@dankito.net>
2024-12-16 03:57:48 +0100

    Using now "Close" instead of "Cancel" for dialogs without confirm button

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/dialogs/PdfInvoiceDataDialog.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/dialogs/PdfInvoiceDataDialog.kt
@@ -1,8 +1,7 @@
 package net.codinux.accounting.ui.dialogs
 
 import androidx.compose.runtime.Composable
-import net.codinux.accounting.resources.Res
-import net.codinux.accounting.resources.invoice_view
+import net.codinux.accounting.resources.*
 import net.codinux.accounting.ui.composables.invoice.PdfInvoiceDataView
 import net.codinux.accounting.ui.config.Colors
 import net.codinux.invoicing.pdf.PdfInvoiceData
@@ -15,6 +14,7 @@ fun PdfInvoiceDataDialog(pdfInvoiceData: PdfInvoiceData, onDismiss: () -> Unit) 
         title = stringResource(Res.string.invoice_view),
         centerTitle = true,
         confirmButtonVisible = false,
+        dismissButtonTitle = stringResource(Res.string.close),
         useMoreThanPlatformDefaultWidthOnSmallScreens = true,
         restrictMaxHeightForFullHeightDialogs = true,
         backgroundColor = Colors.MainBackgroundColor,
--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/dialogs/ViewInvoiceDialog.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/dialogs/ViewInvoiceDialog.kt
@@ -1,8 +1,7 @@
 package net.codinux.accounting.ui.dialogs
 
 import androidx.compose.runtime.Composable
-import net.codinux.accounting.resources.Res
-import net.codinux.accounting.resources.invoice_view
+import net.codinux.accounting.resources.*
 import net.codinux.accounting.ui.composables.invoice.InvoiceView
 import net.codinux.accounting.ui.config.Colors
 import net.codinux.invoicing.model.Invoice
@@ -15,6 +14,7 @@ fun ViewInvoiceDialog(invoice: Invoice, onDismiss: () -> Unit) {
         title = stringResource(Res.string.invoice_view),
         centerTitle = true,
         confirmButtonVisible = false,
+        dismissButtonTitle = stringResource(Res.string.close),
         useMoreThanPlatformDefaultWidthOnSmallScreens = true,
         restrictMaxHeightForFullHeightDialogs = true,
         backgroundColor = Colors.MainBackgroundColor,

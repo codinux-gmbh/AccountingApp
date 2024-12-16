948bb621ea0bf81734508d093385d7d98d9dfa29
dankito <git@dankito.net>
2024-12-16 03:19:27 +0100

    Set app icon (to the same as for Bankmeister) - TODO: find better one

--- composeApp/src/desktopMain/kotlin/net/codinux/accounting/main.kt
+++ composeApp/src/desktopMain/kotlin/net/codinux/accounting/main.kt
@@ -5,12 +5,14 @@ import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import net.codinux.accounting.resources.*
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = stringResource(Res.string.app_name),
+        icon = painterResource(Res.drawable.AppIcon_svg),
         state = WindowState(position = WindowPosition(Alignment.Center), size = DpSize(1000.dp, 800.dp)),
     ) {
         App()

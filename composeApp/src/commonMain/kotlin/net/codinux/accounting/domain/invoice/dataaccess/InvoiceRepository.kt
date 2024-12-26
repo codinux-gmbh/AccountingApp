f313b4245bec5fe8e370b148fee83d6ce80d3e08
dankito <git@dankito.net>
2024-12-26 13:48:45 +0100

    Catching if JSON mapping fails to not write an empty string to file

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/domain/invoice/dataaccess/InvoiceRepository.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/domain/invoice/dataaccess/InvoiceRepository.kt
@@ -3,6 +3,7 @@ package net.codinux.accounting.domain.invoice.dataaccess
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import net.codinux.accounting.domain.invoice.model.HistoricalInvoiceData
+import net.codinux.log.logger
 import java.io.File
 
 class InvoiceRepository(
@@ -11,6 +12,8 @@ class InvoiceRepository(
 ) {
 
     private val historicalDataFile = File(dataDirectory, "invoiceHistoricalData.json")
+
+    private val log by logger()
 
 
     /**
@@ -24,7 +27,12 @@ class InvoiceRepository(
         }
 
     suspend fun saveHistoricalData(data: HistoricalInvoiceData) {
-        jsonMapper.writeValue(historicalDataFile, data)
+        val json = jsonMapper.writeValueAsBytes(data)
+        if (json.isNotEmpty()) {
+            jsonMapper.writeValue(historicalDataFile, data)
+        } else {
+            log.error { "Could not map HistoricalInvoiceData to JSON" }
+        }
     }
 
 }

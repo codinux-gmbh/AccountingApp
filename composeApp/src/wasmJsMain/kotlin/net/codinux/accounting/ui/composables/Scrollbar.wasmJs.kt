084f48b0758dea1137218ab9af6f02f4d81a68a2
dankito <git@dankito.net>
2024-12-30 16:02:06 +0100

    Fixed that Scrollbars are also available for wasmJs

--- composeApp/src/wasmJsMain/kotlin/net/codinux/accounting/ui/composables/Scrollbar.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/net/codinux/accounting/ui/composables/Scrollbar.wasmJs.kt
@@ -1,10 +1,15 @@
 package net.codinux.accounting.ui.composables
 
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.text.TextFieldScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 actual fun VerticalScrollbar(
     state: ScrollableState,
@@ -12,5 +17,14 @@ actual fun VerticalScrollbar(
     reverseLayout: Boolean,
     interactionSource: MutableInteractionSource
 ) {
-    // no-op on WasmJs / not implemented on WasmJs
+
+    if (state is ScrollState) {
+        VerticalScrollbar(rememberScrollbarAdapter(state), modifier, reverseLayout, interactionSource = interactionSource)
+    } else if (state is LazyListState) {
+        VerticalScrollbar(rememberScrollbarAdapter(state), modifier, reverseLayout, interactionSource = interactionSource)
+    } else if (state is LazyGridState) {
+        VerticalScrollbar(rememberScrollbarAdapter(state), modifier, reverseLayout, interactionSource = interactionSource)
+    } else if (state is TextFieldScrollState) {
+        VerticalScrollbar(rememberScrollbarAdapter(state), modifier, reverseLayout, interactionSource = interactionSource)
+    }
 }

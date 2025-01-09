d04b85eab5d31ba4cea7772944c3785455cb0cc2
dankito <git@dankito.net>
2025-01-09 19:39:21 +0100

    Placed text field for IBAN and BIC in one row

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/invoice/BankDetailsForm.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/invoice/BankDetailsForm.kt
@@ -1,10 +1,14 @@
 package net.codinux.accounting.ui.composables.invoice
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import net.codinux.accounting.resources.*
 import net.codinux.accounting.ui.composables.invoice.model.BankDetailsViewModel
+import net.codinux.accounting.ui.config.Style
 
 @Composable
 fun BankDetailsForm(viewModel: BankDetailsViewModel) {
@@ -22,8 +26,10 @@ fun BankDetailsForm(viewModel: BankDetailsViewModel) {
 
     InvoiceTextField(Res.string.name_of_financial_institution, bankName) { viewModel.bankNameChanged(it) }
 
-    InvoiceTextField(Res.string.iban, accountNumber) { viewModel.accountNumberChanged(it) }
+    Row(Modifier.fillMaxWidth().padding(top = Style.FormVerticalRowPadding)) {
+        InvoiceTextField(Res.string.iban, accountNumber, modifier = Modifier.weight(1f)) { viewModel.accountNumberChanged(it) }
 
-    InvoiceTextField(Res.string.bic, bankCode) { viewModel.bankCodeChanged(it) }
+        InvoiceTextField(Res.string.bic, bankCode, modifier = Modifier.width(140.dp).padding(start = 6.dp)) { viewModel.bankCodeChanged(it) }
+    }
 
 }

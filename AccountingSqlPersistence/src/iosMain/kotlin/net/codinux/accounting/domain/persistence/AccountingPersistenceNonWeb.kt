c2e06692ff3d4700d306cc2344a60edc1be1a5a3
dankito <git@dankito.net>
2025-01-09 05:12:08 +0100

    Fixed imports for AccountingPersistenceNonWeb.ios

--- AccountingSqlPersistence/src/iosMain/kotlin/net/codinux/accounting/domain/persistence/AccountingPersistenceNonWeb.kt
+++ AccountingSqlPersistence/src/iosMain/kotlin/net/codinux/accounting/domain/persistence/AccountingPersistenceNonWeb.kt
@@ -1,5 +1,11 @@
 package net.codinux.accounting.domain.persistence
 
+import app.cash.sqldelight.async.coroutines.synchronous
+import app.cash.sqldelight.db.QueryResult
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.db.SqlSchema
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+
 internal actual object AccountingPersistenceNonWeb {
 
     actual fun createSqlDriver(dbName: String, schema: SqlSchema<QueryResult.AsyncValue<Unit>>, version: Long): SqlDriver =

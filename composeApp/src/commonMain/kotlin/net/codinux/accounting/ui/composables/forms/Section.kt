0da8f4b8ab668b43ad165303205826c4dc44e1c1
dankito <git@dankito.net>
2025-01-13 01:40:50 +0100

    Reduced vertical spacing, was a bit too much

--- composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/forms/Section.kt
+++ composeApp/src/commonMain/kotlin/net/codinux/accounting/ui/composables/forms/Section.kt
@@ -1,8 +1,6 @@
 package net.codinux.accounting.ui.composables.forms
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
@@ -10,9 +8,8 @@ import androidx.compose.ui.unit.dp
 import net.codinux.accounting.ui.config.Style
 import org.jetbrains.compose.resources.StringResource
 
-
 @Composable
-fun Section(titleResource: StringResource, spaceBefore: Dp = Style.SectionTopPadding, innerVerticalPadding: Dp = Style.FormVerticalRowPadding, content: @Composable () -> Unit) {
+fun Section(titleResource: StringResource, spaceBefore: Dp = Style.SectionTopPadding, innerVerticalPadding: Dp = 0.dp, content: @Composable () -> Unit) {
     RoundedCornersCard(Modifier.fillMaxWidth().padding(top = spaceBefore)) {
         Column(Modifier.fillMaxWidth().padding(all = Style.FormCardPadding).padding(vertical = innerVerticalPadding)) {
             SectionHeader(titleResource)
